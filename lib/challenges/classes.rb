a142ebd2f4937d9c22a74ee9ae18b27f508e52cb
Grainger <peter.grainger@sage019663.sageukie.adinternal.com>
2018-11-14 09:00:18 +0000

    finished the class challenge

--- lib/challenges/classes.rb
+++ lib/challenges/classes.rb
@@ -1,6 +1,30 @@
 class Ship
-    attr_accessor :shout
+    attr_reader :shout
+    # Add getters and setters for the captain variable
+
     def initialize
-        # Add code here to set the value of shout
+        # Add @shout variable
+        # Don't change the captain!
+        @captain = "Jack Sparrow"
+        @sails = 3
     end
+
+    # LEAVE THE SAILS ALONE
+    def sails
+        @sails
+    end
+
+    # Create a to_s method that summerises this class
+
+    # Make the sails method private
+end
+
+# Change class to inherit from Ship
+class ManOWar
+    def initialize
+        # Call the constructor of the superclass
+        # Change the Captain
+    end
+
+    # Add the shout_out method and access superclass
 end
--- spec/challenges/classes_spec.rb
+++ spec/challenges/classes_spec.rb
@@ -1,8 +1,43 @@
 require "challenges/classes.rb"
 
-describe RSpec.describe Ship, "Class Challenge" do
+describe RSpec.describe Ship, "Class Structure Challenge" do
     it "initialise a class with an attribute shout with a value of AHOY" do
-        ship = p Ship.new
+        ship = Ship.new
         expect(ship.shout).to eq "AHOY"
+    end
+
+    it "Should set the new captain to John Silver" do
+        ship = Ship.new
+        captain = "John Silver"
+        ship.captain = captain
+        expect(ship.captain).to eq captain
+    end
+
+    it "should have a to_s method that summerises the method" do
+        ship = Ship.new
+        expect(ship.to_s).to eq "Captain Jack Sparrow has 3 sails and shouts AHOY!"
+    end
+
+    it "should set sails to be a private method" do
+        ship = Ship.new
+        expect { ship.sails }.to raise_error(NoMethodError)
+    end
+end
+
+describe RSpec.describe ManOWar, "Class Inheritance Challenge" do
+    it "should have a superclass of Ship" do
+        manOWar = ManOWar.new
+        expect(manOWar.class.superclass).to eq Ship
+    end
+
+    it "should have a new captain" do
+        manOWar = ManOWar.new
+        expect(manOWar.captain).to eq "John Silver"
+    end
+
+    it "should have the same shout as the superclass" do
+        ship = Ship.new
+        manOWar = ManOWar.new
+        expect(manOWar.shout_out).to eq ship.shout
     end
 end
